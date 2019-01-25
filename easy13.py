486932397ee8af726c4b46c13ba2867a2cc1f418
Xi Chen <jasonxi89@gmail.com>
2019-01-25 00:39:05 -0800

    1/24

--- easy13.py
+++ easy13.py
@@ -6,3 +6,18 @@ class Solution:
     """
     def strStr(self, source, target):
         # Write your code here
+        if source == "" or target == "":
+            return -1
+        count = 0
+        while True:
+            if source.find(target[0]) == -1:
+                return count
+            count += source.find(target[0])
+            if source[source.find(target[0]):source.find(target[0])+len(target)] == target:
+                return count
+            source = source[source.find(target[0])+1:]
+
+
+
+
+
