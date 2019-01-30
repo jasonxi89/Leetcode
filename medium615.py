1089fa5854801fa7c92dfbf740cf22b41c8f476c
Xi Chen <jasonxi89@gmail.com>
2019-01-30 10:44:43 -0800

    update medium6145

--- medium615.py
+++ medium615.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Solution:
     """
     @param: numCourses: a total of n courses
@@ -14,6 +16,29 @@ class Solution:
     给定 n = 2，先决条件为 [[1,0],[0,1]] 返回 false
     [目标，先学]
     """
+
     def canFinish(self, numCourses, prerequisites):
-        # write your code here
-        
+        # Write your code here
+        edges = {i: [] for i in range(numCourses)}
+        degrees = [0 for i in range(numCourses)]
+        for i, j in prerequisites:
+            edges[j].append(i)
+            degrees[i] += 1
+
+        queue, count = deque([]), 0
+
+        for i in range(numCourses):
+            if degrees[i] == 0:
+                queue.append(i)
+
+        while queue:
+            node = queue.popleft()
+            count += 1
+
+            for x in edges[node]:
+                degrees[x] -= 1
+                if degrees[x] == 0:
+                    queue.append(x)
+
+        return count == numCourses
+
