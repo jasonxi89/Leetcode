1289bd5415bf9468ba06cf4e2814a673dfd9f505
Xi Chen <jasonxi89@gmail.com>
2019-02-07 19:20:19 -0800

    update easy66

---
+++ easy66.py
@@ -0,0 +1,39 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Preorder in ArrayList which contains node values.
+    """
+    def preorderTraversal(self, root):
+        # write your code here
+        result = []
+        self.helper(root)
+        return result
+
+    def helper(self, root):
+        if not root:
+            return
+        self.result.append(root.val)
+        self.helper(root.left)
+        self.helper(root.right)
+
+
+
+    # def preorderTraversal(self, root):
+    #     self.results = []
+    #     self.traverse(root)
+    #     return self.results
+    #
+    # def traverse(self, root):
+    #     if root is None:
+    #         return
+    #     self.results.append(root.val)
+    #     self.traverse(root.left)
+    #     self.traverse(root.right)
