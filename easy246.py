4c0d98474bfe0ee7e99047e53228dd01d1660fbe
Xi Chen <jasonxi89@gmail.com>
2019-02-12 18:40:24 -0800

    updated easy246

---
+++ easy246.py
@@ -0,0 +1,19 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+
+class Solution:
+    """
+    @param: root: the root of binary tree
+    @param: target: An integer
+    @return: all valid paths
+    """
+    def binaryTreePathSum2(self, root, target):
+        # write your code here
+        self.result = []
+        w
