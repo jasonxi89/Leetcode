28d2674f945f2b278cb7830d2ed6b061e59420cb
Xi Chen <jasonxi89@gmail.com>
2019-02-20 14:29:04 -0800

    update medium153

--- medium153.py
+++ medium153.py
@@ -6,3 +6,32 @@ class Solution:
     """
     def combinationSum2(self, num, target):
         # write your code here
+
+        #sort the num for the low -> high
+        num.sort()
+        result = []
+        current = []
+        self.dfs(num,0,target,current,result)
+        return result
+    #递归的定义，
+    def dfs(self, num, startindex, target,current, result):
+        #递归的出口：如果target是0，写入result
+        if target == 0:
+            # result.append(list(current))
+            #两种方法都可以，都是类似于deepcopy
+            result.append(current[:])
+            return
+
+        #遍历0到最后一个数字，看看是不是比target小
+        for i in range(startindex,len(num)):
+            if num[i]> target:
+                break
+            if i!=startindex and num[i-1]==num[i]:
+                continue
+
+            current.append(num[i])
+            self.dfs(num,i+1,target-num[i],current,result)
+            current.pop()
+
+
+
