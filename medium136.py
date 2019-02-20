e6779bc031a46bde5cf0ff3e7084b32e827e1c74
Xi Chen <jasonxi89@gmail.com>
2019-02-19 18:11:00 -0800

    update medium136

--- medium136.py
+++ medium136.py
@@ -1,30 +1,52 @@
-class Solution:
-    """
-    @param: s: A string
-    @return: A list of lists of string
-    """
-    def partition(self, s):
-        # write your code here
-        results = []
-        self.dfs(s,[],results)
-        return results
+# class Solution:
+#     """
+#     @param: s: A string
+#     @return: A list of lists of string
+#     """
+    # def partition(self, s):
+    #     # write your code here
+    #     results = []
+    #     self.dfs(s,[],results)
+    #     return results
+    #
+    #
+    # def dfs(self, s, stringlist, results):
+    #     if len(s) == 0:
+    #         # 深度copy stringlist
+    #         results.append(list(stringlist))
+    #         return
+    #     for i in range(1, len(s)+1):
+    #         prefix = s[:i]
+    #         if self.is_pali(prefix):
+    #             stringlist.append(prefix)
+    #             self.dfs(s[i:],stringlist,results)
+    #             stringlist.pop()
+    #
+    #
+    #
+    # def is_pali(self,s):
+    #     return s == s[::-1]
 
+
+class Solution:
+
+    def partition(self, s):
+        results = []
+        self.dfs(s, [], results)
+        return results
 
     def dfs(self, s, stringlist, results):
         if len(s) == 0:
-            # 深度copy stringlist
             results.append(list(stringlist))
             return
-        for i in range(1, len(s)+1):
+
+        for i in range(1, len(s) + 1):
             prefix = s[:i]
-            if self.is_pali(prefix):
+            if self.is_palindrome(prefix):
                 stringlist.append(prefix)
-                self.dfs(s,stringlist,results)
+                self.dfs(s[i:], stringlist, results)
                 stringlist.pop()
 
-
-
-    def is_pali(self,s):
+    def is_palindrome(self, s):
         return s == s[::-1]
-
 
