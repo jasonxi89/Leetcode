36c8233abb1b81b19b42795a4a8ea4dae86f2bac
Xi Chen <jasonxi89@gmail.com>
2019-02-28 17:40:10 -0800

    updated

--- hard978.py
+++ hard978.py
@@ -67,7 +67,18 @@ class Solution:
                 #deal with last number
                 res += num * sign
                 num = 0
-                
+                sign = [-1 , 1] [ss == "+"]
+            elif ss =="(":
+                stack.append(res)
+                stack.append(sign)
+                res, sign = 0,1
+            elif ss ==")":
+                res += num * sign
+                res *= stack.pop()
+                res += stack.pop()
+                num = 0
+        return res +  num * sign
+
 
 __name__ == "__main__"
 print(Solution.calculate(Solution,"1-(2+3-(4+(5-(1-(2+4-(5+6))))))"))
